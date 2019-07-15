ee46addb664387554f15b5441c26f49b9e2d4a8c
Rich <rnguyen.trieu@gmail.com>
2019-07-15 15:10:21 -0500

    initial push

---
+++ TSR3DSystem/core/insert.py
@@ -0,0 +1,67 @@
+import os
+
+from TSR3DSystem.models import AllProteins, Hierarchy, Comparison
+
+
+def insert_into_all_proteins_table(request):
+    # Hierarchy.objects.get(pk='1GZK').delete()
+    for root, dirs, files in os.walk('C:/Users/rNguy/PycharmProjects/TSR3DSystem/TSR3DSystem/core/mad_triplets/'):
+        for file in files:
+            print(file)
+            f = open(root + file, 'r')
+            protein_ID, _ = Hierarchy.objects.get_or_create(protein_id=str(os.path.splitext(file)[0]))
+            # print( sum(1 for line in f))
+            for counter, line in enumerate(f):
+                # if counter == 400000:
+                if counter == 5000:
+                    break
+                # if counter > 385362:
+                line = line.split('\t')
+                protein = AllProteins(protein_key=int(line[0]), aacd0=str(line[1]), position0=int(line[2]),
+                                      aacd1=str(line[3]), position1=int(line[4]), aacd2=str(line[5]),
+                                      position2=int(line[6]), classT1=int(line[7]), theta=float(line[8]),
+                                      classL1=int(line[9]), maxDist=float(line[10]), x0=float(line[11]),
+                                      y0=float(line[12]), z0=float(line[13]), x1=float(line[14]),
+                                      y1=float(line[15]),
+                                      z1=float(line[16]), x2=float(line[17]), y2=float(line[18]),
+                                      z2=float(line[19]))
+                protein.protein_id = protein_ID
+                protein.save()
+
+
+def similarity_data(request):
+    f = open(
+        'C:/Users/rNguy/Documents/GitKraken/TSR3DSystem/static/other/JaccardSimilarity_134Kinase_29_35_labelled.csv',
+        "r")
+    first_line = f.readline()
+    proteins = first_line.split('\t')
+    proteins.pop()
+    for position, each in enumerate(proteins):
+        print(each)
+        if position != 0:
+            proteins[position] = Hierarchy.objects.get(protein_id=each[0:4])
+    category = 1
+    for line in f:
+        data = line.split('\t')
+        for position, each in enumerate(data):
+            if position != 0:
+                if each == '100.0':
+                    break
+                protein = Comparison(protein_one=proteins[category])
+                protein.protein_two = proteins[position]
+                protein.similarity_value = float(each)
+                protein.save()
+        category += 1
+
+
+def key_occurrence(request):
+    hierarchy = Hierarchy.objects.all()
+    for each in hierarchy:
+        hierarchy_proteins = each.details.all()
+        for protein in hierarchy_proteins:
+            same_keys = hierarchy_proteins.filter(protein_key=protein.protein_key)
+            occurences = len(same_keys)
+            for same_key_protein in same_keys:
+                same_key_protein.key_occurrence = occurences
+                same_key_protein.save()
+            print(protein.protein_key, occurences)
---
+++ TSR3DSystem/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+
+
+class Hierarchy(models.Model):
+    protein_id = models.CharField(max_length=9, primary_key=True, unique=True)
+
+# class Class(models.Model):
+#     protein_class = models.IntegerField(primary_key=True, unique=True)
+#     class_description = models.CharField(max_length=300)
+#
+#
+# class Architecture(models.Model):
+#     protein_architecture = models.IntegerField(primary_key=True, unique=True)
+#     _description = models.CharField(max_length=300)
+#
+#
+# class TopologyFold(models.Model):
+#     protein_topology_fold = models.IntegerField(primary_key=True, unique=True)
+#     topology_fold_description = models.CharField(max_length=300)
+#
+#
+# class HomologySuperFamily(models.Model):
+#     protein_homology_sf = models.IntegerField(primary_key=True, unique=True)
+#     homology_sf_description = models.CharField(max_length=300)
+
+
+class AllProteins(models.Model):
+    protein_id = models.ForeignKey(Hierarchy, on_delete=models.CASCADE, related_name='details')
+    protein_key = models.IntegerField()
+    key_occurrence = models.IntegerField(null=True)
+    aacd0 = models.CharField(max_length=3)
+    position0 = models.IntegerField()
+    aacd1 = models.CharField(max_length=3)
+    position1 = models.IntegerField()
+    aacd2 = models.CharField(max_length=3)
+    position2 = models.IntegerField()
+    classT1 = models.IntegerField()
+    theta = models.FloatField()
+    classL1 = models.IntegerField()
+    maxDist = models.FloatField()
+    x0 = models.FloatField()
+    y0 = models.FloatField()
+    z0 = models.FloatField()
+    x1 = models.FloatField()
+    y1 = models.FloatField()
+    z1 = models.FloatField()
+    x2 = models.FloatField()
+    y2 = models.FloatField()
+    z2 = models.FloatField()
+
+    # class Meta:
+    #     index_together = ['protein_id', 'protein_key']
+
+
+class Comparison(models.Model):
+    protein_one = models.ForeignKey(Hierarchy, on_delete=models.CASCADE, related_name="comparison_one")
+    protein_two = models.ForeignKey(Hierarchy, on_delete=models.CASCADE, related_name="comparison_two")
+    similarity_value = models.FloatField()
+
+# class Position(models.Model):
+#     Protein_ID = models.ForeignKey(
+#         PROTEIN_HIERARCHY,
+#         to_field='Protein_ID',
+#         on_delete=models.CASCADE)
+#     AminoAcid_Name = models.CharField(max_length=20)
+#     Seq_ID = models.CharField(max_length=5, null=True)
+#     X_COORD = models.FloatField(default=0.0)
+#     Y_COORD = models.FloatField(default=0.0)
+#     Z_COORD = models.FloatField(default=0.0)
