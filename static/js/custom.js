6b04a8b756d3daec728453405ac78ab8fd958dd3
Rich <rnguyen.trieu@gmail.com>
2019-07-22 19:28:25 -0500

    Styling Update

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,3 +2,15 @@
 // $(":button[type='submit']").click(function(){
 //     $('.preloader').toggle(); // set duration in brackets
 // });
+
+
+
+// select multiple validation
+function select_validation() {
+    if ($(":selected").length < 2) {
+        swal({text: "Select at least two proteins!", button: false});
+        return false;
+    } else {
+        return true;
+    }
+}
