3925ac014f349319d76c8e32bbc4efaee8a875f0
Rich <rnguyen.trieu@gmail.com>
2019-07-29 20:52:27 -0500

    added code to compare

--- TSR3DSystem/compare.py
+++ TSR3DSystem/compare.py
@@ -19,9 +19,77 @@ class CompareByProteinID(ListView):
         return context
 
 
+# def compare_by_protein_id_result(request):
+#     start = time.clock()
+#     context = {}
+#
+#     if request.method == "POST":
+#         row_val = []
+#         protein_compared = request.POST["list1"]
+#         protein_list = request.POST.getlist("list2")
+#         context['protein_compared'] = protein_compared
+#
+#         if not protein_list:
+#             queryset = Hierarchy.objects.all()
+#             for query in queryset:
+#                 protein_list.append(query.pk)
+#
+#        if protein_compared not in protein_list:
+#            protein_list.append(protein_compared)
+#
+#         for protein in protein_list:
+#             queryset = Comparison.objects.filter(Q(protein_one=protein_compared) | Q(protein_two=protein_compared))
+#
+#             if protein == protein_compared:
+#                 continue
+#
+#             similarity_value = queryset[0].similarity_value
+#
+#             # hierarchy_result = Hierarchy.objects.get(pk=protein)
+#
+#             # class_result = CLASS_DESCRIPTION.objects.get(
+#             #     Class=hierarchy_result.Class_id)
+#
+#             # architecture_result = ARCHITECTURE_DESCRIPTION.objects.get(
+#             #     Architecture=hierarchy_result.Architecture_id)
+#
+#             # topology_result = TOPOLOGYFOLD_DESCRIPTION.objects.get(
+#             #     TopologyFold=hierarchy_result.TopologyFold_id)
+#
+#             # homology_result = HOMOLOGYSUPERFAMILY_DESCRIPTION.objects.get(
+#             # HomologySuperfamily=hierarchy_result.HomologySuperfamily_id)
+#
+#             row_val.append({
+#                 'ProtId': protein,
+#                 'similarity': similarity_value,
+#                 # 'class': hierarchy_result.Class_id,
+#                 # 'class_desc': class_result.DescriptionOfClass,
+#                 # 'archi': hierarchy_result.Architecture_id,
+#                 # 'archi_desc': architecture_result.DescriptionOfArchitecture,
+#                 # 'topfold': hierarchy_result.TopologyFold_id,
+#                 # 'topfold_desc': topology_result.DescriptionOfTopologyFold,
+#                 # 'homsup': hierarchy_result.HomologySuperfamily_id,
+#                 # 'homsup_desc': homology_result.DescriptionOfHomologySuperfamily
+#             })
+#
+#         context['protein_details_list'] = sorted(
+#             row_val, key=itemgetter('similarity'), reverse=True)
+#     else:
+#         context['no_result_found'] = True
+#     end = time.clock()
+#     context['time'] = round(end - start, 4)
+#     context.update(nav_bar())
+#     return TemplateResponse(request, 'compare/compare_by_pid_result.html', context)
+
+######################
+#    TITLI'S CODE    #
+######################
 def compare_by_protein_id_result(request):
     start = time.clock()
     context = {}
+
+    if request.method == "GET":
+        context['no_result_found'] = True
 
     if request.method == "POST":
         row_val = []
@@ -34,30 +102,33 @@ def compare_by_protein_id_result(request):
             for query in queryset:
                 protein_list.append(query.pk)
 
-        # if protein_compared not in protein_list:
-        #     protein_list.append(protein_compared)
+        if protein_compared not in protein_list:
+            protein_list.append(protein_compared)
 
         for protein in protein_list:
-            queryset = Comparison.objects.filter(Q(protein_one=protein_compared) | Q(protein_two=protein_compared))
+            similarity_info_queryset = Comparison.objects.filter(
+                protein_one=protein_compared,
+                protein_two=protein)
 
-            if protein == protein_compared:
+            if not similarity_info_queryset:
                 continue
 
-            similarity_value = queryset[0].similarity_value
+            similarity_value = similarity_info_queryset[0].Similarity_Value
 
-            # hierarchy_result = Hierarchy.objects.get(pk=protein)
-
+            # hierarchy_result = PROTEIN_HIERARCHY.objects.get(
+            #     Protein_ID=protein)
+            #
             # class_result = CLASS_DESCRIPTION.objects.get(
             #     Class=hierarchy_result.Class_id)
-
+            #
             # architecture_result = ARCHITECTURE_DESCRIPTION.objects.get(
             #     Architecture=hierarchy_result.Architecture_id)
-
+            #
             # topology_result = TOPOLOGYFOLD_DESCRIPTION.objects.get(
             #     TopologyFold=hierarchy_result.TopologyFold_id)
-
+            #
             # homology_result = HOMOLOGYSUPERFAMILY_DESCRIPTION.objects.get(
-            # HomologySuperfamily=hierarchy_result.HomologySuperfamily_id)
+            #     HomologySuperfamily=hierarchy_result.HomologySuperfamily_id)
 
             row_val.append({
                 'ProtId': protein,
@@ -74,13 +145,10 @@ def compare_by_protein_id_result(request):
 
         context['protein_details_list'] = sorted(
             row_val, key=itemgetter('similarity'), reverse=True)
-    else:
-        context['no_result_found'] = True
+
     end = time.clock()
     context['time'] = round(end - start, 4)
-    context.update(nav_bar())
     return TemplateResponse(request, 'compare/compare_by_pid_result.html', context)
-
 
 class CompareByHierarchy(ListView):
     model = Hierarchy
