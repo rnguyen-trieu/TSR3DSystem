18ca1470b9f836cf6ca0dab55622c37d02d7348e
Rich <rnguyen.trieu@gmail.com>
2019-11-04 22:25:40 -0600

    panda inserting

--- TSR3DSystem/core/insert.py
+++ TSR3DSystem/core/insert.py
@@ -39,9 +39,34 @@ def insert_into_all_proteins_table(request):
                     protein.save()
 
 
+def insert_into_all_proteins_table_pandas(request):
+    import pandas as pd
+    from sqlalchemy import create_engine
+    engine = create_engine('postgresql://richn:komiisawsome@tsr3dsystem.cmix.louisiana.edu:5432/tsr3d_kinase')
+
+    for root, dirs, files in os.walk('/mnt/space/tsr3duser/Kinase'):
+        for file in files:
+            print(file)
+            f = open(root + file, 'r')
+            protein_ID, _ = Hierarchy.objects.get_or_create(protein_id=str(os.path.splitext(file)[0]))
+
+    for root, dirs, files in os.walk('/mnt/space/tsr3duser/Kinase'):
+        for file in files:
+            print(file)
+            chunk_list = []
+            df_chunk = pd.read_csv(root + file, sep='\t', names=['protein_key', 'aacd0', 'position0', 'aacd1', 'position1', 'aacd2', 'position2',
+                                       'classT1', 'theta', 'classL1', 'maxDist', 'x0', 'y0', 'z0', 'x1', 'y1', 'z1',
+                                       'x2', 'y2', 'z2'], chunksize=1000000)
+            for chunk in df_chunk:
+                chunk_list.append(chunk[chunk.maxDist <= 20])
+            df_concat = pd.concat(chunk_list)
+            df_concat['protein_id'] = str(os.path.splitext(file)[0])
+            df_concat.to_sql('tsr3dsystem_allproteins', engine, if_exists='append')
+
+
 def similarity_data(request):
     f = open(
-        'C:/Users/rNguy/Documents/GitKraken/TSR3DSystem/static/other/JaccardSimilarity_134Kinase_29_35_labelled.csv',
+        "C:/Users/rNguy/Documents/GitKraken/TSR3DSystem/TSR3DSystem/core/other/JaccardSimilarity_134Kinase_29_35_labelled.csv",
         "r")
     first_line = f.readline()
     proteins = first_line.split('\t')
