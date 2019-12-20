344209c2c61df2c6372f99e4e9a3f09274f788dd
rich <rnguyen.trieu@gmail.com>
2019-12-19 22:03:11 -0600

    updated with dataframes

--- TSR3DSystem/core/background_tasks.py
+++ TSR3DSystem/core/background_tasks.py
@@ -1,7 +1,7 @@
 import csv
 import time
 import uuid
-import pyfpgrowth
+import pandas as pd
 from background_task import background
 
 from django.core.mail import send_mail
@@ -55,65 +55,77 @@ def class_filter(email, protein_class, max_distance, min_support, min_confidence
     itemsets, rules = apriori(key_csv, min_support=min_support, min_confidence=min_confidence)
     print(itemsets)
 
-    # FP Growth Portion
-    # patterns = pyfpgrowth.find_frequent_patterns(keys, min_support)
-    # rules = pyfpgrowth.generate_association_rules(patterns, min_confidence)
-    # print(patterns)
-
     # Search Classes with Apriori Results
     protein_classes = [class_name for class_name in DATABASES if class_name != 'default' and class_name != protein_class ]
 
-    iter_itemsets = iter(itemsets)
-    next(iter_itemsets)
-    next(iter_itemsets)
+    if len(itemsets) != 0:
+        iter_itemsets = iter(itemsets)
+        next(iter_itemsets)
+        next(iter_itemsets)
 
-    filtered_dict = {}
-    for itemset in iter_itemsets:
-        for item in itemsets[itemset]:
-            classes = {}
-            for class_name in protein_classes:
+        column_names = ['ItemSets: Count']
+        [column_names.extend([x, x + '_proteins']) for x in protein_classes]
+        df = pd.DataFrame(columns=column_names)
+        float_columns = [column for column in column_names[1::2]]
 
-                item_set_list = [protein_key[:-2] for protein_key in item]
-                # when filtering 4 lines below might need to make it unique for faster processing
+        for itemset in iter_itemsets:
+            for item in itemsets[itemset]:
+                classes = {}
+                for class_name in protein_classes:
+                    protein_itemset_list = []
+                    for protein in Hierarchy.objects.using(class_name).all():
 
-                counted_item_set_list = Counter(item_set_list)
-                protein_itemset_list = []
-                for protein in Hierarchy.objects.using(class_name).all():
-                    protein_key_list = AllProteins.objects.using(class_name).filter(protein_key__in=item_set_list,
-                                                                                    protein_id_id=protein.pk).values_list(
-                                                                                    'protein_key', flat=True)
-                    protein_key_counter = Counter(protein_key_list)
+                        item_set_list = [protein_key[:-2] for protein_key in item]
+                        counted_item_set_list = Counter(item_set_list)
 
-                    count_checker = Counter(
-                        {key: protein_key_counter[int(key)] - value for key, value in counted_item_set_list.items()})
+                        protein_key_list = AllProteins.objects.using(class_name).filter(protein_key__in=item_set_list,
+                                                                                        protein_id_id=protein.pk).values_list(
+                                                                                        'protein_key', flat=True)
+                        protein_key_counter = Counter(protein_key_list)
 
-                    # Checks to make sure that it has all of the keys.
-                    # If there are any negative values than it does not have all of the items in the itemset
-                    protein_has_set = not any(elem < 0 for elem in count_checker.values())
+                        count_checker = Counter(
+                            {key: protein_key_counter[int(key)] - value for key, value in counted_item_set_list.items()})
 
-                    if protein_has_set:
-                        protein_itemset_list.append(protein.pk)
-                # percentage = len(protein_itemset_list)/Hierarchy.objects.using(class_name).count()
-                # if percentage >= min_support:
-                #     classes.update({class_name: {'percentage': percentage, 'proteins': protein_itemset_list}})
-                classes.update({class_name: {'percentage': len(protein_itemset_list)/Hierarchy.objects.using(class_name).count(),
-                                             'proteins': protein_itemset_list}})
-            filtered_dict.update({str(item): classes})
-    print(filtered_dict)
+                        # Checks to make sure that it has all of the keys.
+                        # If there are any negative values than it does not have all of the items in the itemset
+                        protein_has_set = not any(elem < 0 for elem in count_checker.values())
 
-    msg_html = render_to_string('email_template.html', {'filtered_dict': filtered_dict,
-                                                        'protein_classes': protein_classes,
-                                                        'time': round(time.clock() - start, 4),
-                                                        'protein_class': protein_class,
-                                                        'max_distance': max_distance,
-                                                        'min_support': min_support,
-                                                        'min_confidence': min_confidence})
+                        if protein_has_set:
+                            protein_itemset_list.append(protein.pk)
+                    # percentage = len(protein_itemset_list)/Hierarchy.objects.using(class_name).count()
+                    # if percentage >= min_support:
+                    #     classes.update({class_name: {'percentage': percentage, 'proteins': protein_itemset_list}})
+                    #classes.update({class_name: {'percentage': len(protein_itemset_list)/Hierarchy.objects.using(class_name).count(),
+                    #                             'proteins': protein_itemset_list}})
+                    classes.update({class_name: len(protein_itemset_list)/Hierarchy.objects.using(class_name).count(),
+                                    class_name + '_proteins': protein_itemset_list})
+                classes.update({'ItemSets: Count': str(counted_item_set_list)[7:]})
+                df = df.append(pd.DataFrame(classes), ignore_index=True, sort=False)
+            df[float_columns] = df[float_columns].astype('float32')
 
-    send_mail(
-        'TSR3DSystem Results',
-        '',
-        settings.EMAIL_HOST_USER,
-        [email],
-        html_message=msg_html,
-        fail_silently=False,
-    )
+        print(df.to_html)
+
+        msg_html = render_to_string('email_template.html', {'filtered_dict': df.to_html,
+                                                            'protein_classes': protein_classes,
+                                                            'time': round(time.clock() - start, 4),
+                                                            'protein_class': protein_class,
+                                                            'max_distance': max_distance,
+                                                            'min_support': min_support,
+                                                            'min_confidence': min_confidence})
+
+        send_mail(
+            'TSR3DSystem Results',
+            '',
+            settings.EMAIL_HOST_USER,
+            [email],
+            html_message=msg_html,
+            fail_silently=False,
+        )
+    else:
+        send_mail(
+            'TSR3DSystem Results',
+            'There were no itemsets that met the minimum support and minimum confidence',
+            settings.EMAIL_HOST_USER,
+            [email],
+            fail_silently=False,
+        )
